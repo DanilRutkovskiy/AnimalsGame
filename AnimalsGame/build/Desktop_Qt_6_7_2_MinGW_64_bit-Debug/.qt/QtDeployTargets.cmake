0982d1bf13b290175477c48d8171a60c61253f56
Danil.Rutkovskiy <rutkovskiy.danil@mail.ru>
2024-06-24 18:55:33 +0500

    Deleted Build

--- AnimalsGame/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_AnimalsGame_FILE D:/repo/AnimalsGame/AnimalsGame/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/AnimalsGame.exe)
-set(__QT_DEPLOY_TARGET_AnimalsGame_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_AnimalsGame_RUNTIME_DLLS D:/libs/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;D:/libs/Qt/6.7.2/mingw_64/bin/Qt6Sql.dll;D:/libs/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;D:/libs/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
